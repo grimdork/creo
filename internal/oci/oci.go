3177ddae5ec7865befe7cb7af3474537409c6ce5
Urethramancer <github@grimdork.net>
2026-06-17 04:14:07 +0200

    Split oci.go: extract 4 layer-creation functions into layers.go
    Moved certsLayer, binaryLayer, directoryLayer, and extraFileLayer to
    internal/oci/layers.go. Removed the now-unused archive/tar import from
    oci.go.

--- internal/oci/oci.go
+++ internal/oci/oci.go
@@ -1,7 +1,6 @@
 package oci
 
 import (
-	"archive/tar"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
@@ -294,14 +293,6 @@ func saveToCache(img v1.Image, path string) error {
 	return nil
 }
 
-func certsLayer(caCert string) (v1.Layer, error) {
-	data, err := os.ReadFile(caCert)
-	if err != nil {
-		return nil, err
-	}
-	return layerFromBytes(caCertPath, data, 0644)
-}
-
 // FetchCACert downloads the Mozilla CA certificate bundle from curl.se.
 func FetchCACert() ([]byte, error) {
 	resp, err := httpClient.Get(caCertURL)
@@ -337,112 +328,4 @@ func WriteTarball(img v1.Image, path, tag string) error {
 	return tarball.WriteToFile(path, ref, img)
 }
 
-func binaryLayer(binary, appDir, name string) (v1.Layer, error) {
-	data, err := os.ReadFile(binary)
-	if err != nil {
-		return nil, err
-	}
-	return layerFromBytes(filepath.Join(appDir, name), data, 0755)
-}
 
-func directoryLayer(srcDir, appDir string) (v1.Layer, error) {
-	if appDir == "" {
-		appDir = "/app"
-	}
-
-	tmp, err := os.CreateTemp("", "creo-layer-*.tar")
-	if err != nil {
-		return nil, fmt.Errorf("creating temp file: %w", err)
-	}
-	tmpPath := tmp.Name()
-
-	tw := tar.NewWriter(tmp)
-
-	walkErr := filepath.WalkDir(srcDir, func(path string, d os.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		rel, err := filepath.Rel(srcDir, path)
-		if err != nil {
-			return err
-		}
-		if rel == "." {
-			return nil
-		}
-		target := filepath.Join(appDir, rel)
-
-		fi, err := d.Info()
-		if err != nil {
-			return err
-		}
-
-		header, err := tar.FileInfoHeader(fi, "")
-		if err != nil {
-			return err
-		}
-		header.Name = target
-		header.ModTime = time.Time{}
-
-		if fi.IsDir() {
-			header.Name += "/"
-		}
-
-		if err := tw.WriteHeader(header); err != nil {
-			return err
-		}
-		if !fi.IsDir() {
-			data, err := os.ReadFile(path)
-			if err != nil {
-				return err
-			}
-			if _, err := tw.Write(data); err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-	tw.Close()
-	tmp.Close()
-	if walkErr != nil {
-		os.Remove(tmpPath)
-		return nil, fmt.Errorf("taring %q: %w", srcDir, walkErr)
-	}
-
-	return tarball.LayerFromOpener(func() (io.ReadCloser, error) {
-		return os.Open(tmpPath)
-	})
-}
-
-func extraFileLayer(src, dst string, isURL bool) (v1.Layer, error) {
-	var data []byte
-	var mode int64 = 0644
-
-	if isURL {
-		resp, err := httpClient.Get(src)
-		if err != nil {
-			return nil, fmt.Errorf("downloading %s: %w", src, err)
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode != http.StatusOK {
-			return nil, fmt.Errorf("downloading %s: %s", src, resp.Status)
-		}
-		data, err = io.ReadAll(resp.Body)
-		if err != nil {
-			return nil, fmt.Errorf("reading %s: %w", src, err)
-		}
-	} else {
-		fi, err := os.Stat(src)
-		if err != nil {
-			return nil, fmt.Errorf("stat %s: %w", src, err)
-		}
-		if fi.Mode()&0111 != 0 {
-			mode = 0755
-		}
-		data, err = os.ReadFile(src)
-		if err != nil {
-			return nil, fmt.Errorf("reading %s: %w", src, err)
-		}
-	}
-
-	return layerFromBytes(dst, data, mode)
-}
